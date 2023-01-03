7c4939ac2b51647cfad6c27ec5b4e18a8d41e03e
ChrisBafax <chrisbafaro@icloud.com>
2023-01-03 12:02:29 +0100

    0.0.1 Creation of project

---
+++ src/main/java/it/java/course/esercitazione1/Esercitazione1Application.java
@@ -0,0 +1,13 @@
+package it.java.course.esercitazione1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercitazione1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Esercitazione1Application.class, args);
+    }
+
+}
