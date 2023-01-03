7cd5b06d7c81f636908c1d1dc1f168af26ad2280
ChrisBafax <chrisbafaro@icloud.com>
2023-01-03 14:15:28 +0100

    1.0.0 Add Course class model. Add CourseRepository.
    Fixed pom dependencies.
    Edited application.properties.

---
+++ src/main/java/it/java/course/esercitazione1/model/Course.java
@@ -0,0 +1,31 @@
+package it.java.course.esercitazione1.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "course")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private long id;
+
+
+    @Getter
+    @Setter
+    @Column(name = "name")
+    private String name;
+
+    @Getter
+    @Setter
+    @Column(name = "description")
+    private String description;
+
+}
